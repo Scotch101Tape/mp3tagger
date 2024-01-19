8b54f8372261911d862cc1381d493180de24576c
Scotch101Tape <scotch101tape@gmail.com>
2024-01-18 20:19:57 -0600

    Clean up bpm.py

--- mp3tagger/scripts/bpm.py
+++ mp3tagger/scripts/bpm.py
@@ -1,23 +1,17 @@
-from tkinter import BitmapImage
-
-
+# Get the filter string for bpm attributes
 def get_bpm_filter(bpm_list):
+    # Get the low and high bpms
     bpmLow = bpm_list[0]
-    filter = f"((NOT BPM LESS {bpmLow}"
-
     bpmHi = bpm_list[1]
-    filter = filter + f" AND NOT BPM GREATER {bpmHi})"
 
+    # Include a secondary bpm range, either double or halved
+    if bpmLow > 80:
+        bpmLow2 = int(bpmLow / 2)
+        bpmHi2 = int(bpmHi / 2)
+    else:
+        bpmLow2 = int(bpmLow * 2)
+        bpmHi2 = int(bpmHi * 2)
 
-    # "SECONDARY BPM RANGE (DOUBLED OR HALVED)"
-    if type(bpmLow) == int:
-        # print(bpmLow)
-        if bpmLow > 80:
-            bpmLow2 = int(bpmLow / 2)
-            bpmHi2 = int(bpmHi / 2)
-        else:
-            bpmLow2 = int(bpmLow * 2)
-            bpmHi2 = int(bpmHi * 2)
-        if bpmLow2:
-            filter = filter + f" OR (NOT BPM LESS {bpmLow2} AND NOT BPM GREATER {bpmHi2})) AND "
+    filter = f"((NOT BPM LESS {bpmLow} AND NOT BPM GREATER {bpmHi}) OR (NOT BPM LESS {bpmLow2} AND NOT BPM GREATER {bpmHi2})) AND "
+
     return filter
