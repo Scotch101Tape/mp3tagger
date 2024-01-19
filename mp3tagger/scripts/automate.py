2e639d3fe02408a67788957fe94856cdb105b0f3
Scotch101Tape <scotch101tape@gmail.com>
2024-01-18 19:41:11 -0600

    Clean up automate.py

--- mp3tagger/scripts/automate.py
+++ mp3tagger/scripts/automate.py
@@ -1,89 +1,33 @@
 from subprocess import Popen
 from pywinauto import Desktop
 from pywinauto.application import Application
-# import pywinauto
 import os
 
+# Types the filter text into the mp3tag window
 def automate(filter):
-
-    # FASTER BUT CAN'T FIND SCORE TOTAL
     try:
-        # connect again with this faster way to find the filter
+        # Connect to the mp3tag window
         app = Application().connect(path=os.environ.get("MP3TAG_PATH"))
     except:
-        print('are you sure mp3 tag is open before you connect?')
+        # TODO: send this to a gui messaging system
+        print('Are you sure mp3tag is open before you connect?')
         exit()
 
+    # Open the window
     window =  app.top_window()
-    
-
     window.maximize()
-    # print(dlg_spec.menu())
 
-    # #  TAKES A LOOOOOOOOOOOONG TIME IF THERE ARE MANY SONGS IN THE VIEW
-    # window.print_control_identifiers()
+    # Get filter ready
+    filter = filter.replace( ')' ,  '{)}') \
+        .replace( '(' ,  '{(}') \
+        .replace( ' ' ,  '{ }') \
+        .replace( '%' ,  '{%}')
 
-    # sys.exit()
-
-    # filter.replace( '(' ,  r'\(' )
-    filter = filter.replace( ')' ,  '{)}')
-    filter = filter.replace( '(' ,  '{(}')
-    filter = filter.replace( ' ' ,  '{ }')
-    filter = filter.replace( '%' ,  '{%}')
-
-    # filter = filter[3:5]
-    print(filter)
-    # https://stackoverflow.com/questions/44369703/attributeerror-windowspecification-class-has-no-typekeys-method
-
-
-    # # add the filter text to the filter
-    # window.child_window(best_match="ComboBox").type_keys(filter)
-
-    # FOR NOT BACKEND = UAI
-       # Clear the existing content in the ComboBox
+    # Clear the existing content in the ComboBox
     combo_box = window['Filte&r:ComboBox']
     combo_box.set_focus()
     combo_box.type_keys('^a')  # Ctrl+A to select all
     combo_box.type_keys('{BACKSPACE}')  # Clear the selected text
+
+    # Send the filter to mp3tag
     window['Filte&r:ComboBox'].send_chars(filter)
-
-    
-
-    # window['Filte&r:ComboBox'].type_keys(filter)
-
-    # window['Filte&r:ComboBox'].type_keys('')
-    # filter = filter.replace('{','').replace('}', '')
-    # chars = [c for c in filter if not c == '(' and not c == ')']
-    # chars = [c for c in filter if not c == '{' and not c == '}']
-    # print(chars)
-    # # chars = ['{', '(', '}', '{', '(', '}', 'N', 'O', 'T', '{', ' ', '}', 'B', 'P', 'M', '{', ' ', '}', 'L', 'E', 'S', 'S', '{', ' ', '}', '8', '0', '{', ' ', '}', 'A', 'N', 'D', '{', ' ', '}', 'N', 'O', 'T', '{', ' ', '}', 'B', 'P', 'M', '{', ' ', '}', 'G', 'R', 'E', 'A', 'T', 'E', 'R', '{', ' ', '}', '1', '0', '0', '{', ')', '}', '{', ' ', '}', 'O', 'R', '{', ' ', '}', '{', '(', '}', 'N', 'O', 'T', '{', ' ', '}', 'B', 'P', 'M', '{', ' ', '}', 'L', 'E', 'S', 'S', '{', ' ', '}', '1', '6', '0', '{', ' ', '}', 'A', 'N', 'D', '{', ' ', '}', 'N', 'O', 'T', '{', ' ', '}'
-    
-    # window['Filte&r:ComboBox'].send_chars(chars, with_spaces=True)
-
-
-
-# Popen(r'os.environ.get("MP3TAG_PATH")', shell=True)
-    
-    
-    # dlg = Desktop(backend="uia").mp3tag
-    # dlg.wait('visible')
-    
-    # app = Application().start(r'os.environ.get("MP3TAG_PATH")')
-
-
-    # # CONNECT AGAIN WITH THIS SLOWER BACK END THAT FIND'S THE SCORETOTAL. THIS ONE FAILS CUZ IT'S TOO SLOW IF MANY SONGS IN WINDOW
-    # try:
-    #     app = Application(backend="uia").connect(path=r'os.environ.get("MP3TAG_PATH")')
-    # except:
-    #     print('are you sure mp3 tag is open before you connect?')
-    #     sys.exit()
-
-    # # sort by scoreTotal
-    # # window.scoreTotal.print_control_identifiers()
-    # window.child_window(title="scoreTotal", auto_id="HeaderItem 1", control_type="HeaderItem").click_input()
-
-    # app.Properties.print_control_identifiers()
-    # sleep(3)
-    # dialogs = app.windows()
-    # pprint(dialogs)
-    # sys.exit()
